454bca95e6a5c0ea75c349e5df6661fff73666a3
Nick Moore <nickmoore_1@live.com>
2019-11-10 21:42:39 -0500

    Starting React Course / First app

---
+++ app1/app1/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    const name = 'Nick'
+    return (
+      <div className="App">
+        React
+        {name}
+      </div>
+    )
+  }
+}
+
+export default App;
