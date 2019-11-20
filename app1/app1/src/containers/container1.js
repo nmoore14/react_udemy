784f42086ad9454c35144e1659c5b66f959e9088
Nick Moore <nickmoore_1@live.com>
2019-11-20 12:56:03 -0500

    working through forms and starting redux

--- app1/app1/src/containers/container1.js
+++ app1/app1/src/containers/container1.js
@@ -3,45 +3,28 @@ import Component1 from '../functional/component1';
 
 class Container1 extends Component {
 
-	Arr1 = [
-		{
-			id: 1,
-			text: 'text 1',
-			number: 1
-		},
-		{
-			id: 2,
-			text: 'text 2',
-			number: 2
-		},
-		{
-			id: 3,
-			text: 'text 3',
-			number: 3
-		},
-		{
-			id: 4,
-			text: 'text 4',
-			number: 4
-		},
-		{
-			id: 5,
-			text: 'text 5',
-			number: 5
-		}
-	]
+	state = {
+		value: ''
+	}
 
-	RenderListItem = (props) => (
-		<div>
-			{props.item.text}
-			<p>{props.item.number}</p>
-		</div>
+	handleChange = (event) => (
+		this.setState({ value: event.target.value })
 	)
+
+	handleSubmit = (event) => {
+		event.preventDefault();
+		console.log(this.state.value)
+	}
 
 	render() {
 		return (
 			<div>
-				{this.Arr1.map((item) => ( <this.RenderListItem key={item.id} item={item} />))}
+				{this.state.value}
+				<form onSubmit={this.handleSubmit}>
+					<label>Name</label>
+					<input id="name" onChange={this.handleChange} type="text" />
+					<button type="submit">Submit</button>
+				</form>
 			</div>
 		)
 	}
